459e1b088f9ca5984cf263aa2c358ceaeb70eefb
Kennadee <kennadee.ray@gmail.com>
2020-10-01 14:17:39 -0500

    get request succesfully implemented for Users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,8 +2,13 @@ const { User } = require('../models');
 
 const userController = {
     // get all users
-    getAllUsers() {
-
+    getAllUsers(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.ststus(400).json(err);
+            });
     },
 
     // get one user by id
