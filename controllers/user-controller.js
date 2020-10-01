4b9020d911257298e5e8c1546dccac35290fe424
Kennadee <kennadee.ray@gmail.com>
2020-10-01 14:12:49 -0500

    create user route added and tested

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const userController = {
     // get all users
     getAllUsers() {
-        
+
     },
 
     // get one user by id
@@ -12,8 +12,10 @@ const userController = {
     },
 
     // create a new user
-    createUser() {
-
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
     },
 
     // update user by id
